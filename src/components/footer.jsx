e8ede2e4d3f0505b4a26fe5a2261166adb7aa437
Harshit Jain <harshitostwal1234@gmail.com>
2024-03-19 22:42:48 +0530

    Updated Ui/Ux of Footer & Header

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,10 +3,10 @@ import SocialMedia from './social-media'
 
 function Footer() {
     return (
-        <footer className="h-[500px] gradient-Color rounded-t-[100px] w-full flex flex-col gap-10 items-center justify-center">
-            <h1 className="text-lg font-semibold">STAY UP TO DATE WITH OUR NEWS AND NOTIFICATIONS</h1>
+        <footer className="h-full p-5 py-20 text-white md:text-black text-center gradient-Color lg:rounded-t-[200px] w-full flex flex-col gap-10 items-center justify-center">
+            <h1 className="font-semibold">STAY UP TO DATE WITH OUR NEWS AND NOTIFICATIONS</h1>
             <SocialMedia />
-            <p className="font-medium">Copyright © HACKWITHINDIA | All Rights Resverd</p>
+            <p className="text-sm font-medium md:text-base">Copyright © HACKWITHINDIA | All Rights Resverd</p>
         </footer>
     )
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,18 +1,25 @@
+import { GanttChart } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 
 function Header() {
     return (
-        <div className="flex items-center justify-around p-2 bg-white">
-            <Link href={"/"} className="flex flex-col cursor-pointer">
-                <span className="text-4xl font-bold">HACK</span>
-                <span className="text-lg font-semibold">WITH INDIA</span>
-            </Link>
-            <div className="flex items-center justify-center gap-10">
+        <div className="flex items-center justify-between w-full px-5 py-5 bg-white lg:justify-around">
+            <div className="flex items-center justify-center gap-5">
+                <span className="block sm:hidden"><GanttChart /></span>
+                <Link href={"/"} className="flex flex-col leading-none cursor-pointer">
+                    <span className="text-3xl font-bold sm:text-4xl">HACK</span>
+                    <span className="font-semibold tracking-tight sm:tracking-normal sm:text-lg">WITH INDIA</span>
+                </Link>
+            </div>
+            <div className="items-center hidden gap-10 sm:flex">
                 <Link href={""} className="font-medium duration-200 hover:text-violet-700">About</Link>
                 <Link href={""} className="font-medium duration-200 hover:text-violet-700">Gallery</Link>
                 <Link href={""} className="font-medium duration-200 hover:text-violet-700">Contact</Link>
                 <Link href={""} className="px-8 py-4 font-semibold bg-amber-500 rounded-tr-2xl rounded-bl-2xl">Join Us</Link>
+            </div>
+            <div className="flex sm:hidden">
+                <Link href={""} className="px-6 py-3 font-semibold sm:py-4 sm:px-8 bg-amber-500 rounded-tr-2xl rounded-bl-2xl">Join Us</Link>
             </div>
         </div>
     )
