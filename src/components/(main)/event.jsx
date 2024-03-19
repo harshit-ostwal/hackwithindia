cbfb9be07b2780559dbe60ddea5b3c15df5822a4
Harshit Jain <harshitostwal1234@gmail.com>
2024-03-20 00:42:17 +0530

    Updated image height size

--- src/components/(main)/event.jsx
+++ src/components/(main)/event.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Event() {
     return (
         <div className="flex items-center justify-center w-full min-h-full p-3 py-20 lg:p-40 via-black bg-gradient-to-tr from-gradient-middle to-gradient-start">
-            <Image src={"/Event.png"} width={800} height={800} quality={100} loading="lazy" />
+            <Image src={"/Event.png"} width={800} height={766} quality={100} loading="lazy" />
         </div>
     )
 }
