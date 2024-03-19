a6c3c11a786c72d34eb3180fdd1107670596e0e1
Harshit Jain <harshitostwal1234@gmail.com>
2024-03-19 22:15:31 +0530

    Updated header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,10 +4,10 @@ import React from 'react'
 function Header() {
     return (
         <div className="flex items-center justify-around p-2 bg-white">
-            <div className="flex flex-col">
-                <h1 className="text-4xl font-bold">HACK</h1>
-                <h2 className="text-lg font-semibold">WITH INDIA</h2>
-            </div>
+            <Link href={"/"} className="flex flex-col cursor-pointer">
+                <span className="text-4xl font-bold">HACK</span>
+                <span className="text-lg font-semibold">WITH INDIA</span>
+            </Link>
             <div className="flex items-center justify-center gap-10">
                 <Link href={""} className="font-medium duration-200 hover:text-violet-700">About</Link>
                 <Link href={""} className="font-medium duration-200 hover:text-violet-700">Gallery</Link>
