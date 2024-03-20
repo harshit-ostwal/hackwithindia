ed1a96449cca655b4c773f216900f16d829fcdd7
Harshit Jain <harshitostwal1234@gmail.com>
2024-03-20 18:55:00 +0530

    Updated Header, TechStack, Images & Installed new packages

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,27 +1,53 @@
+"use client"
 import { GanttChart } from 'lucide-react'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
+import { Slant as Hamburger } from 'hamburger-react'
 
 function Header() {
+
+    const [Menu, setMenu] = useState();
+
+    const Toggle = () => {
+        setMenu(!Menu);
+    }
+
     return (
-        <div className="flex items-center justify-between w-full p-5 lg:px-20">
-            <div className="flex items-center justify-center text-white gap-5">
-                <span className="block sm:hidden"><GanttChart /></span>
-                <Link href={"/"} className="flex flex-col leading-none cursor-pointer">
-                    <span className="text-3xl font-bold sm:text-4xl">HACK</span>
-                    <span className="font-semibold tracking-tight sm:tracking-normal sm:text-lg">WITH INDIA</span>
-                </Link>
+        <>
+            <div className="flex items-center justify-between w-full p-5 lg:px-20">
+                <div className="flex items-center justify-center text-white gap-5">
+                    <button onClick={Toggle} className={`${Menu ? "fixed top-5 left-5" : "flex"} sm:hidden z-[2000]`}>
+                        <Hamburger color={Menu ? "#000" : "#fff"} duration={0.8} size={24} rounded toggled={Menu} toggle={setMenu} />
+                    </button>
+                    <button onClick={Toggle} className={`${Menu ? "flex" : "hidden"} scale-0`}>
+                        <Hamburger color={Menu ? "#000" : "#fff"} duration={0.8} size={24} rounded toggled={Menu} toggle={setMenu} />
+                    </button>
+                    <Link href={"/"} className="flex flex-col leading-none cursor-pointer">
+                        <span className="text-3xl font-bold sm:text-4xl">HACK</span>
+                        <span className="font-semibold tracking-tight sm:tracking-normal sm:text-lg">WITH INDIA</span>
+                    </Link>
+                </div>
+                <div className="items-center hidden gap-10 sm:flex">
+                    <Link href={"/About"} className="font-medium text-white duration-200 hover:text-amber-500">About</Link>
+                    <Link href={"/Gallery"} className="font-medium text-white duration-200 hover:text-amber-500">Gallery</Link>
+                    <Link href={"/Contact"} className="font-medium text-white duration-200 hover:text-amber-500">Contact</Link>
+                    <Link href={""} className="px-8 py-4 font-semibold bg-amber-500 rounded-tr-2xl rounded-bl-2xl">Join Us</Link>
+                </div>
+                <div className="flex sm:hidden">
+                    <Link href={""} className="px-6 py-3 font-semibold sm:py-4 sm:px-8 bg-amber-500 rounded-tr-2xl rounded-bl-2xl">Join Us</Link>
+                </div>
             </div>
-            <div className="items-center hidden gap-10 sm:flex">
-                <Link href={"/About"} className="font-medium text-white duration-200 hover:text-amber-500">About</Link>
-                <Link href={"/Gallery"} className="font-medium text-white duration-200 hover:text-amber-500">Gallery</Link>
-                <Link href={"/Contact"} className="font-medium text-white duration-200 hover:text-amber-500">Contact</Link>
-                <Link href={""} className="px-8 py-4 font-semibold bg-amber-500 rounded-tr-2xl rounded-bl-2xl">Join Us</Link>
+
+            <div className={`h-screen fixed z-[1600] bg-white text-black top-0 transition-all items-center justify-center duration-1000 ease-in-out w-[100%] sm:w-[70%] md:w-[25%] ${Menu ? "left-0" : "-left-[100%]"}`
+            }>
+                <div className="flex flex-col gap-5 p-10 items-center h-full w-full  justify-center">
+                    <Link href={"/"} onClick={Toggle} className="font-medium duration-200 hover:text-amber-500">Home</Link>
+                    <Link href={"/About"} onClick={Toggle} className="font-medium duration-200 hover:text-amber-500">About</Link>
+                    <Link href={"/Gallery"} onClick={Toggle} className="font-medium duration-200 hover:text-amber-500">Gallery</Link>
+                    <Link href={"/Contact"} onClick={Toggle} className="font-medium duration-200 hover:text-amber-500">Contact</Link>
+                </div>
             </div>
-            <div className="flex sm:hidden">
-                <Link href={""} className="px-6 py-3 font-semibold sm:py-4 sm:px-8 bg-amber-500 rounded-tr-2xl rounded-bl-2xl">Join Us</Link>
-            </div>
-        </div>
+        </>
     )
 }
 
