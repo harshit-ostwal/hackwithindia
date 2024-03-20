4c0e5409236e6f2835791c9ce008243192ea0ad5
Harshit Jain <harshitostwal1234@gmail.com>
2024-03-20 16:34:35 +0530

    Created History Section

---
+++ src/components/(main)/history.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function History() {
+    return (
+        <div className="text-white via-black bg-gradient-to-tl p-5 py-40 flex flex-col justify-center items-center gap-14 md:gap-20 from-gradient-middle to-gradient-start min-h-screen">
+            <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold">Our History</h1>
+            <p className="flex flex-col gap-2 max-w-7xl lg:text-lg">
+                <span>
+                    The primary aim of this hackathon would be to inspire young brains to develop their technical, and team collaboration skills, and encourage them to foster a heightened sense of responsibility towards society, instilling them to channel their creative ideas towards betterment of the community.
+                </span>
+                <span>
+                    There will be 2 rounds in the hackathon. On the first day, Round 1 will begin with participants giving an abstract and presentation on their idea, followed by Round 2 of the mentorship process. Only the shortlisted teams will be in round 2 which is prototyping. After the results of round 2 finalists will give the presentation to the judges on the second day and the winner of the contest will be declared.
+                </span>
+                <span>
+                    We are expected to gather more than 800 participants from all over other universities. We would also be providing them with food and basic amenities from the sponsors fund. Additionally, we'd be contacting a number of well-known figures in industry and education.
+                </span>
+            </p>
+        </div>
+    )
+}
+
+export default History
