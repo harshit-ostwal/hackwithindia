416be4e1e5036ca3b0b0fceac2e8bc214c747a66
Harshit Jain <harshitostwal1234@gmail.com>
2024-03-21 02:58:00 +0530

    Added Countdown Functionality

--- src/components/(main)/intro.jsx
+++ src/components/(main)/intro.jsx
@@ -1,8 +1,37 @@
+"use client"
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 function Intro() {
+
+    const [timeLeft, setTimeLeft] = useState({
+        days: '00',
+        hours: '00',
+        minutes: '00',
+        seconds: '00'
+    });
+
+    useEffect(() => {
+        const targetDate = new Date('2024-03-30T23:59:59');
+        const timer = setInterval(() => {
+            const difference = targetDate - new Date();
+            if (difference > 0) {
+                setTimeLeft({
+                    days: padLeft(Math.floor(difference / (1000 * 60 * 60 * 24))),
+                    hours: padLeft(Math.floor((difference / (1000 * 60 * 60)) % 24)),
+                    minutes: padLeft(Math.floor((difference / 1000 / 60) % 60)),
+                    seconds: padLeft(Math.floor((difference / 1000) % 60)),
+                });
+            }
+        }, 1000);
+        return () => clearInterval(timer);
+    }, []);
+
+    function padLeft(value) {
+        return value.toString().padStart(2, '0');
+    }
+
     return (
         <div className="flex flex-col items-center w-full min-h-full gap-20 p-3 py-20 md:p-40 justify-evenly xl:flex-row">
             <div className="flex flex-col items-center justify-center gap-10 xl:items-start">
@@ -17,15 +46,27 @@ function Intro() {
                 <Link href={""} className="px-6 py-3 font-semibold text-black bg-amber-500 rounded-tr-2xl rounded-bl-2xl">Join Us</Link>
             </div>
             <div className="relative">
-                <Image width={600} height={332} quality={100} loading="lazy" src="/AppleVisionPro.png" alt="Apple Vision Pro" />
-                <div className="text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[65%] flex font-bold text-4xl lg:text-7xl">
-                    <h1>00</h1>
-                    <span>:</span>
-                    <h1>00</h1>
-                    <span>:</span>
-                    <h1>00</h1>
-                    <span>:</span>
-                    <h1>00</h1>
+                <Image width={600} height={332} quality={100} loading="lazy" src="/AppleVisionPro.png" alt="AppleVisionPro" />
+                <div className="text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[65%] flex font-bold text-3xl sm:text-5xl 2xl:text-7xl">
+                    <div className="flex flex-col gap-2 items-center justify-center">
+                        <h1>{timeLeft.days}</h1>
+                        <h1 className="text-xs sm:text-sm lg:text-base">DAYS</h1>
+                    </div>
+                    <span className="whitespace-pre"> : </span>
+                    <div className="flex flex-col gap-2 items-center justify-center">
+                        <h1>{timeLeft.hours}</h1>
+                        <h1 className="text-xs sm:text-sm lg:text-base">HOURS</h1>
+                    </div>
+                    <span className="whitespace-pre"> : </span>
+                    <div className="flex flex-col gap-2 items-center justify-center">
+                        <h1>{timeLeft.minutes}</h1>
+                        <h1 className="text-xs sm:text-sm lg:text-base">MINUTES</h1>
+                    </div>
+                    <span className="whitespace-pre"> : </span>
+                    <div className="flex flex-col gap-2 items-center justify-center">
+                        <h1>{timeLeft.seconds}</h1>
+                        <h1 className="text-xs sm:text-sm lg:text-base">SECONDS</h1>
+                    </div>
                 </div>
             </div>
         </div>
