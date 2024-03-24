352fdc6c435c440fa79185a669b36c37ce72c129
Harshit Jain <harshitostwal1234@gmail.com>
2024-03-24 17:34:28 +0530

    Updated Image Loading Type

--- src/components/(about)/gallery2.jsx
+++ src/components/(about)/gallery2.jsx
@@ -4,9 +4,9 @@ import React from 'react'
 function Gallery2() {
     return (
         <div className="grid w-full h-full grid-cols-1 gap-10 md:grid-cols-2 xl:grid-cols-3">
-            <Image src={"/4.jpg"} width={800} height={800} className="rounded-tr-3xl rounded-bl-3xl aspect-video" alt='About' quality={100} />
-            <Image src={"/5.jpg"} width={800} height={800} className="rounded-tr-3xl rounded-bl-3xl aspect-video" alt='About' quality={100} />
-            <Image src={"/6.jpg"} width={800} height={800} className="rounded-tr-3xl rounded-bl-3xl aspect-video" alt='About' quality={100} />
+            <Image src={"/4.jpg"} width={800} height={800} loading="lazy" className="rounded-tr-3xl rounded-bl-3xl aspect-video" alt='About' quality={100} />
+            <Image src={"/5.jpg"} width={800} height={800} loading="lazy" className="rounded-tr-3xl rounded-bl-3xl aspect-video" alt='About' quality={100} />
+            <Image src={"/6.jpg"} width={800} height={800} loading="lazy" className="rounded-tr-3xl rounded-bl-3xl aspect-video" alt='About' quality={100} />
         </div>
     )
 }
