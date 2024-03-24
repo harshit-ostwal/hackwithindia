834d31de54ddb3a793f198f34d3c852497392d95
Harshit Jain <harshitostwal1234@gmail.com>
2024-03-24 16:37:19 +0530

    Updated Images

--- src/components/(main)/techstack.jsx
+++ src/components/(main)/techstack.jsx
@@ -3,46 +3,54 @@ import React from 'react'
 
 const Images = [
   {
-    title: "OPEN INNOVATION",
-    imageUrl: "/21.png"
+    title: "ARTIFICIAL INTELLIGENCE",
+    imageUrl: "/35.png"
   },
   {
-    title: "SMART AGRICULTURE",
-    imageUrl: "/22.png"
+    title: "WEB DEVELOPMENT",
+    imageUrl: "/36.png"
   },
   {
-    title: "HEALTH & FITNESS",
-    imageUrl: "/23.png"
+    title: "APP DEVELOPMENT",
+    imageUrl: "/37.png"
   },
   {
-    title: "FINTECH",
-    imageUrl: "/24.png"
+    title: "AUGMENTED REALITY (AR)",
+    imageUrl: "/38.png"
   },
   {
-    title: "ENVIRONMENTAL IMPACT",
-    imageUrl: "/25.png"
+    title: "BRAIN COMPUTER INTERFACE",
+    imageUrl: "/39.png"
   },
   {
-    title: "SMART EDUCATION",
-    imageUrl: "/26.png"
+    title: "EDGE / CLOUD COMPUTING",
+    imageUrl: "/40.png"
+  },
+  {
+    title: "VIRUTAL REALITY (VR)",
+    imageUrl: "/41.png"
+  },
+  {
+    title: "INTERNET OF THINGS",
+    imageUrl: "/42.png"
   }
 ]
 
 function TechStack() {
   return (
-    <div className="text-white via-black flex items-center justify-center bg-gradient-to-tl from-gradient-start to-gradient-middle w-full min-h-screen">
-      <div className="flex flex-col max-w-7xl w-full p-5 justify-center items-center lg:items-stretch gap-20 min-h-screen">
-        <h1 className="text-4xl sm:text-6xl font-bold">Tech Stack</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2  xl:grid-cols-3 gap-20 items-center justify-center">
+    <div className="flex items-center justify-center w-full min-h-screen p-5 pt-40 text-white via-black bg-gradient-to-tl from-gradient-start to-gradient-middle">
+      <div className="flex flex-col items-center justify-center w-full min-h-screen gap-20 p-5 max-w-7xl lg:items-stretch">
+        <h1 className="text-4xl font-bold sm:text-6xl">Tech Stack</h1>
+        <div className="grid items-center justify-center grid-cols-1 gap-20 sm:grid-cols-2 xl:grid-cols-3">
           {Images.map((data, index) => (
             <div key={index} className="flex flex-col items-center justify-center gap-10">
               <Image src={data.imageUrl} alt={data.title} quality={100} loading="lazy" width={128} height={128} className="w-20 md:w-32" />
-              <h1 className="font-medium text-sm lg:text-lg tracking-widest">{data.title}</h1>
+              <h1 className="text-sm font-medium tracking-widest lg:text-lg">{data.title}</h1>
             </div>
           ))}
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 
