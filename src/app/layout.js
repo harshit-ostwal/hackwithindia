dce042762957b3b1b55f71a29ecbac5119537ffe
Harshit Jain <harshitostwal1234@gmail.com>
2024-03-25 11:58:04 +0530

    Alligned Code

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,18 +25,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link
-          rel="icon"
-          href="/icon?<generated>"
-          type="image/<generated>"
-          sizes="<generated>"
-        />
-        <link
-          rel="apple-touch-icon"
-          href="/apple-icon?<generated>"
-          type="image/<generated>"
-          sizes="<generated>"
-        />
+        <link rel="icon" href="/icon?<generated>" type="image/<generated>" sizes="<generated>" />
+        <link rel="apple-touch-icon" href="/apple-icon?<generated>" type="image/<generated>" sizes="<generated>" />
       </head>
       <body suppressHydrationWarning={true} className={`${outfit.variable} font-Outfit antialiased`}>
         <SmoothScroll>
