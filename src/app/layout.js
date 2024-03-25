9509af0965e450f00fc6a74ae252d1bfda14fadf
Harshit Jain <harshitostwal1234@gmail.com>
2024-03-25 11:57:08 +0530

    Updated Favicons

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,10 +24,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
       </head>
       <body suppressHydrationWarning={true} className={`${outfit.variable} font-Outfit antialiased`}>
         <SmoothScroll>
