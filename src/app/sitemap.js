53dd0f55c3e8c6c5fad3e6cdfeea51fe6ea044d7
Harshit Jain <harshitostwal1234@gmail.com>
2024-04-04 12:17:42 +0530

    Updated Sitemap.js

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -3,31 +3,31 @@ export default function sitemap() {
         {
             url: 'https://hackwithindia.live',
             lastModified: new Date(),
-            changefreq: "Monthly",
+            changeFreq: "Monthly",
             priority: 1.0,
         },
         {
             url: 'https://hackwithindia.live/Guidelines',
             lastModified: new Date(),
-            changefreq: "Monthly",
+            changeFreq: "Monthly",
             priority: 0.8,
         },
         {
             url: 'https://hackwithindia.live/About',
             lastModified: new Date(),
-            changefreq: "Monthly",
+            changeFreq: "Monthly",
             priority: 0.8,
         },
         {
             url: 'https://hackwithindia.live/Gallery',
             lastModified: new Date(),
-            changefreq: "Monthly",
+            changeFreq: "Monthly",
             priority: 0.8,
         },
         {
             url: 'https://hackwithindia.live/Contact',
             lastModified: new Date(),
-            changefreq: "Monthly",
+            changeFreq: "Monthly",
             priority: 0.8,
         },
     ]
