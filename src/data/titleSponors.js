fae8ccad2b60f500b955465f93fe50b4b8b33f5a
Harshit Jain <harshitostwal1234@gmail.com>
2024-05-18 21:38:09 +0530

    Updated Title Sponors

--- src/data/titleSponors.js
+++ src/data/titleSponors.js
@@ -1,6 +1,6 @@
 import MXLogo from "../../public/Partners/mx-logo-black.png"
 import Freshen from "../../public/Main/Freshen-Logo.png"
-import Hive from "../../public/Main/Hive.png"
+import Hive from "../../public/Main/Hive.svg"
 
 export const TitleSponorData = [
     {
@@ -14,14 +14,14 @@ export const TitleSponorData = [
         titleSponsorLogo: Hive,
         titleSponsorName: "Hive",
         titleSponsorCategory: "Power Sponsor",
-        titleSponsorDesc: "Enjoy our refreshing drinks to stay energized and hydrated as you innovate. Let's code and stay fresh together!",
-        titleSponsorLink: "https://www.logitech.com/en-in/mx/master-series.html",
+        titleSponsorDesc: "Hive is a DPoS powered blockchain & cryptocurrency. Fast. Scalable. Powerful. Hive has a thriving ecosystem of dapps, communities & individuals.",
+        titleSponsorLink: "https://hive.io",
     },
-        {
+    {
         titleSponsorLogo: Freshen,
         titleSponsorName: "Freshen",
         titleSponsorCategory: "Beverages Sponsor",
         titleSponsorDesc: "Enjoy our refreshing drinks to stay energized and hydrated as you innovate. Let's code and stay fresh together!",
-        titleSponsorLink: "https://www.logitech.com/en-in/mx/master-series.html",
+        titleSponsorLink: "https://www.freshendrinks.com",
     }
 ]
