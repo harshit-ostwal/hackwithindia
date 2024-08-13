6d57ed04658f380793b5177dc3a756755a440806
Harshit Jain <harshitostwal1234@gmail.com>
2024-08-14 01:55:32 +0530

    Updated PastEvents

--- src/app/(routes)/Gallery/page.jsx
+++ src/app/(routes)/Gallery/page.jsx
@@ -10,7 +10,7 @@ import { PastEvents } from '@/data/pastEvents';
 function page() {
 
   return (
-    <div className="flex flex-col items-center w-full min-h-screen gap-20 p-2 overflow-hidden">
+    <div className="flex flex-col items-center justify-center w-full min-h-screen gap-20 p-2 overflow-hidden">
 
       <div className="flex flex-col items-center max-w-5xl gap-5 text-center">
         <BoxReveal boxColor={"#5046e6"} duration={0.5}>
@@ -25,21 +25,21 @@ function page() {
         </BoxReveal>
       </div>
 
-      <div className="h-full gap-14 lg:columns-2 2xl:columns-3 columns-1">
+      <div className="h-full gap-14 lg:columns-2 2xl:columns-3">
         {
           PastEvents.map((data, index) => (
             <Link href={`Gallery/${data.title}`} key={index}>
               <ShineBorder
-                className="w-[95%] sm:w-[500px] p-8 rounded-md mb-14 flex flex-col gap-14"
+                className="w-[340px] sm:w-[500px] p-6 md:p-8 rounded-md mb-14 flex flex-col gap-14"
                 color={["#A07CFE", "#FE8FB5", "#FFBE7B"]}
               >
                 <Image src={data.thumbanil} alt={data.title} quality={100} fetchPriority="high" loading="eager" priority={true} />
                 <div className="flex flex-col w-full gap-4">
-                  <SparklesText className="text-4xl font-extrabold tracking-tighter" text={data.title} />
-                  <p className="text-sm text-neutral-400">{data.description}</p>
-                  <div className="flex items-center justify-between w-full">
-                    <h1 className="text-sm font-semibold text-neutral-400">{data.location}</h1>
-                    <h1 className="text-sm font-semibold text-neutral-400">{data.date}</h1>
+                  <SparklesText className="text-2xl font-extrabold tracking-tighter sm:text-4xl" text={data.title} />
+                  <p className="text-sm font-thin text-neutral-100">{data.description}</p>
+                  <div className="flex flex-col justify-between w-full md:flex-row">
+                    <h1 className="text-sm font-semibold text-neutral-300">{data.location}</h1>
+                    <h1 className="text-sm font-semibold text-neutral-300">{data.date}</h1>
                   </div>
                 </div>
               </ShineBorder>
