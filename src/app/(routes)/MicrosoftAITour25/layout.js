109356209511bf80c702a99a8c98a4d1c11b47db
Harshit Jain <harshitostwal1234@gmail.com>
2024-12-24 02:51:22 +0530

    Updated

---
+++ src/app/(routes)/MicrosoftAITour25/layout.js
@@ -0,0 +1,7 @@
+export default function MicrosoftAITour25({ children }) {
+    return (
+        <div className="absolute inset-0 min-h-screen bg-black z-[1000]">
+            {children}
+        </div>
+    )
+}
